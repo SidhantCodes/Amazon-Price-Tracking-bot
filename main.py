1cd13d0f2b95744356ed4066f5142a0ce6d3c91a
Sidhant Mishra <mishrasidhant01@gmail.com>
2024-01-31 09:53:58 +0530

    Can now send mails to alert the user when there is a price drop

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
-
+import smtplib
+from dotenv import load_dotenv
+import os
+load_dotenv()
+url = input("Enter the url of the amazon product here: ")
+tgt_price = float(input("Enter the target amount: "))
 site = requests.get(
-    "https://www.amazon.in/Seagate-Expansion-1TB-External-HDD/dp/B08ZJDWTJ1/ref=sr_1_3?crid=1UUDKSXYKFXEE&keywords=hard+disk&qid=1706671937&sprefix=hard+disk%2Caps%2C298&sr=8-3", headers={"User-Agent":"Mozilla/5.0", "Accept-Language":"en-US,en;q=0.5"}
+    url, headers={"User-Agent":"Mozilla/5.0", "Accept-Language":"en-US,en;q=0.5"}
     )
 # print(site.text)
 soup = BeautifulSoup(site.content, "lxml")
 # print(soup.prettify())
 product_title = soup.find(name="span", id="productTitle").getText().strip()
-print(product_title)
+print("Product Title: "+product_title)
 
-product_price = soup.find(name="span", class_="a-price-whole").getText().strip()[:-1]
-print(product_price)
+product_price = float((soup.find(name="span", class_="a-price-whole").getText().strip()[:-1]).replace(',',''))
 
+print("Current Price: "+str(product_price))
+
+if product_price<tgt_price:
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        sender_email = os.getenv("EMAIL")
+        sender_password = os.getenv("PASSWORD")
+        receiver_email = os.getenv("RECEIVER_EMAIL")
+        message = f"{product_title} is now {product_price}"
+        connection.starttls()
+        result = connection.login(sender_email, sender_password)
+        connection.sendmail(
+            from_addr=sender_email,
+            to_addrs=receiver_email,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )
